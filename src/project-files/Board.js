ad674aee5cf9aaa346c2f1d5954a579e77e4e24a
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2024-08-02 10:05:16 +0330

    now we have king condition

--- src/project-files/Board.js
+++ src/project-files/Board.js
@@ -1,10 +1,69 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { BoardPartContext } from "../App";
+
+function ShowLegalMove({ block, handleChangePiece }) {
+  const boardBlocks = useContext(BoardPartContext);
+  let content = "";
+
+  if (block.color === "black") {
+    if (block.isFilled === false) {
+      if (boardBlocks.pieces[boardBlocks.activePiece] !== undefined) {
+        if (boardBlocks.pieces[boardBlocks.activePiece].isKing === true) {
+          if (
+            block.num === boardBlocks.activePiece - 7 ||
+            block.num === boardBlocks.activePiece - 9 ||
+            block.num === boardBlocks.activePiece + 7 ||
+            block.num === boardBlocks.activePiece + 9
+          ) {
+            content = (
+              <span className="legal--move" onClick={handleChangePiece}></span>
+            );
+          }
+        } else if (
+          boardBlocks.pieces[boardBlocks.activePiece].isKing === false
+        ) {
+          if (boardBlocks.pieces[boardBlocks.activePiece].color === "red") {
+            if (
+              block.num === boardBlocks.activePiece + 7 ||
+              block.num === boardBlocks.activePiece + 9
+            ) {
+              content = (
+                <span
+                  className="legal--move"
+                  onClick={handleChangePiece}
+                ></span>
+              );
+            }
+          } else if (
+            boardBlocks.pieces[boardBlocks.activePiece].color === "black"
+          ) {
+            if (
+              block.num === boardBlocks.activePiece - 7 ||
+              block.num === boardBlocks.activePiece - 9
+            ) {
+              content = (
+                <span
+                  className="legal--move"
+                  onClick={handleChangePiece}
+                ></span>
+              );
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return content;
+}
 
 function Piece({ block }) {
   const boardBlocks = useContext(BoardPartContext);
 
   function handleMakeActive() {
+    // only player who his turn is should play
+    if (boardBlocks.playerTurn !== boardBlocks.pieces[block.num].color) return;
+
     let newObj = boardBlocks.pieces;
     let PieceKeys = Object.keys(boardBlocks.pieces);
     // after each click i want to have only one active piece
@@ -15,26 +74,38 @@ function Piece({ block }) {
 
     boardBlocks.setPieces({ ...newObj });
     boardBlocks.setActivePiece(block.num);
-    // showLegalMove();
   }
 
-  // function showLegalMove() {}
+  function handleChangePiece() {
+    // making a new blocks array
+    let newPieceObj = boardBlocks.pieces;
+    newPieceObj[block.num] = {
+      ...newPieceObj[boardBlocks.activePiece],
+      isActive: false,
+    };
+    delete newPieceObj[boardBlocks.activePiece];
+    boardBlocks.setPieces({ ...newPieceObj });
+    // making a new pieces object
+    let newBlockArray = boardBlocks.blocks;
+    newBlockArray[boardBlocks.activePiece - 1].isFilled = false;
+    newBlockArray[block.num - 1].isFilled = true;
+    boardBlocks.setBlocks([...newBlockArray]);
+    // now there is no selected piece
+    boardBlocks.setActivePiece("");
+    // change turn
+    boardBlocks.setPlayerTurn(
+      boardBlocks.playerTurn === "red" ? "black" : "red"
+    );
+  }
 
   return (
     <div className={`block block--${block.color}`}>
       <span style={{ position: "absolute", top: "0", left: "0" }}>
         {block.num}
       </span>
-      {block.color === "black" &&
-      block.isFilled === false &&
-      (block.num === boardBlocks.activePiece - 7 ||
-        block.num === boardBlocks.activePiece - 9 ||
-        block.num === boardBlocks.activePiece + 7 ||
-        block.num === boardBlocks.activePiece + 9) ? (
-        <span className="legal--move"></span>
-      ) : (
-        ""
-      )}
+
+      <ShowLegalMove block={block} handleChangePiece={handleChangePiece} />
+
       {block.isFilled && (
         <span
           onClick={handleMakeActive}
